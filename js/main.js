150afe640b2d0d5167789520aefd925e43c95f4e
Valerii Radko <valerii.radko@gmail.com>
2018-03-07 14:00:33 +0100

    Change the structure of CV and update the information

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 jQuery(document).ready(function($) {
-  console.log("Thank you for checking my CV code");
+  console.log("Thank you for checking Valerii Radko CV");
+  $('.level-bar-inner').css('width', '0');
+
+
+  $('.level-bar-inner').each(function() {
+    var itemWidth = $(this).data('level');
+
+    $(this).animate({
+      width: itemWidth
+    }, 1000);
+  });
+
 });
